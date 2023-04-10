b90780d42a292902ba6261c8663b1e8ffae816f7
Hasnathasan <hasnatoooooooo@gmail.com>
2023-04-11 01:09:31 +0600

    _redirects file added

---
+++ src/components/HomeTitlePage/HomeTitle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import manPic from '../../assets/All Images/P3OLGJ1 copy 1.png'
+
+const HomeTitle = () => {
+    return (
+        <div className="bg-violet-50 px-5">
+            <div className="container mx-auto grid grid-cols-1  md:grid-cols-2 gap-5 justify-center items-center">
+                <div className="order-1">
+                    <h1 className="text-4xl sm:text-7xl leading-tight font-extrabold">One Step Closer To Your <span className="text-indigo-400">Dream Job</span></h1>
+                    <p className="text-gray-500 my-5">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className="btn border-none">Get Started</button>
+                </div>
+                <div className="-order-1 sm:order-2">
+                    <img className="w-full" src={manPic} alt="" />
+                </div>
+            </div>
+        </div>
+        
+    );
+};
+
+export default HomeTitle;
---
+++ src/components/Job-List/JobList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const JobList = ({job}) => {
+    const {id, img, category, available} = job;
+    return (
+        <div className="bg-violet-50 p-8 rounded-lg">
+            <img className="bg-slate-200 w-14 h-14 mb-3 p-3 rounded" src={img} alt="" />
+            <h3 className="text-xl font-semibold">{category}</h3>
+            <h4 className="text-gray-700 mt-1 font-medium">{available} jobs Available</h4>
+        </div>
+    );
+};
+
+export default JobList;
