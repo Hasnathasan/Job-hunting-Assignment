d7fe2d6ed5c6f44f3e8c78105a6cae3e6b929df7
Hasnathasan <hasnatoooooooo@gmail.com>
2023-04-11 01:03:34 +0600

    AppliedJobs, Blog, Statistics file added

---
+++ src/components/AppliedJobs/ApplliedJobs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ApplliedJobs = () => {
+    return (
+        <div>
+            <div className="bg-violet-50 py-10">
+            <h2 className="text-3xl text-center font-bold">Applied Jobs</h2>
+            </div>
+        </div>
+    );
+};
+
+export default ApplliedJobs;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import HTforStatistics from '../HomeTitleOfStatistics/HTforStatistics';
+
+const Statistics = () => {
+    
+    return (
+        <div>
+            <HTforStatistics></HTforStatistics>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Statistics;
