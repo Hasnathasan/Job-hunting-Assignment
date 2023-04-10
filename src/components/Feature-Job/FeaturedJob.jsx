20bdb91a3524d4f301c58066bcf50158f6948127
Hasnathasan <hasnatoooooooo@gmail.com>
2023-04-11 03:17:23 +0600

    featured section customized

--- src/components/Feature-Job/FeaturedJob.jsx
+++ src/components/Feature-Job/FeaturedJob.jsx
@@ -6,11 +6,11 @@ const FeaturedJob = ({job}) => {
     const {jobTitle, company, companyPhoto, jobtype, address, salary} = job;
     console.log(job);
     return (
-        <div className="border border-slate-200 p-9">
+        <div className="border border-slate-200 p-9 rounded-lg">
             <img src={companyPhoto} alt="" />
             <h3 className="text-2xl font-bold my-3">{jobTitle}</h3>
             <h4 className="text-lg text-gray-600 font-semibold mb-2">{company}</h4>
-            <div className="my-3">
+            <div className="my-5">
                 <span className="px-2 py-1 text-base text-violet-500 font-bold border border-violet-400 rounded">{jobtype}</span>
                 <span className="px-2 py-1 text-base text-violet-500 font-bold border border-violet-400 rounded ml-3">Full Time</span>
             </div>
@@ -24,6 +24,7 @@ const FeaturedJob = ({job}) => {
                     <h4>{salary}</h4>
                 </div>
             </div>
+            <button className="btn border-none text-white mt-5">View Details</button>
         </div>
     );
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,10 +4,19 @@ import JobList from '../Job-List/JobList';
 import { useLoaderData } from 'react-router-dom';
 import FeaturedJob from '../Feature-Job/FeaturedJob';
 
+let jobQuantity = 4;
 const Main = () => {
-    let jobQuantity = 4;
+    const [featuredJobs, setFeaturedJobs] = useState([])
     const jobs = useLoaderData();
-    const fourJobs = jobs.filter(job => job.id <= jobQuantity);
+    const showAll = () => {
+        setFeaturedJobs(jobs)
+    }
+    useEffect(() => {
+        const Jobs = jobs.filter(job => job.id <= jobQuantity);
+        setFeaturedJobs(Jobs);
+    },[])
+    
+
     const [jobCategory, setJobCategory] = useState([])
     useEffect( () => {
         fetch('jobCategory.json')
@@ -19,17 +28,20 @@ const Main = () => {
             <HomeTitle></HomeTitle>
             <div className="my-32 container mx-auto">
                 
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
+                <div className="grid grid-cols-1 p-4 md:p-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7">
                     {
                         jobCategory.map(job => <JobList key={job.id} job={job}></JobList>)
                     }
                 </div>
             </div>
-            <div className="container mx-auto">
+            <div className="container mx-auto my-10">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     {
-                        fourJobs.map(job => <FeaturedJob key={job.id} job={job}></FeaturedJob>)
+                        featuredJobs.map(job => <FeaturedJob key={job.id} job={job}></FeaturedJob>)
                     }
+                </div>
+                <div className="text-center">
+                    <button onClick={showAll} className={featuredJobs.length === 6 ? "hidden" : "btn border-none text-white mt-5"}>Show More</button>
                 </div>
             </div>
             
