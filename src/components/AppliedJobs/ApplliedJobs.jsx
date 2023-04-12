fc756e0e5d2fd83685729bb877748a1312867a1f
Hasnathasan <hasnatoooooooo@gmail.com>
2023-04-12 23:45:43 +0600

    Filter by funtionality added

--- src/components/AppliedJobs/ApplliedJobs.jsx
+++ src/components/AppliedJobs/ApplliedJobs.jsx
@@ -5,6 +5,7 @@ import RenderJob from "../RenderAppliedJobs/RenderJob";
 
 const ApplliedJobs = () => {
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [jobWithType, setJobWithType] = useState([])
   const featuredJobs = useLoaderData();
   const localJobsId = getfeaturedJob();
   useEffect(() => {
@@ -14,15 +15,33 @@ const ApplliedJobs = () => {
       selectedJobs.push(job);
     }
     setAppliedJobs(selectedJobs);
+    setJobWithType(selectedJobs)
   }, []);
-
+  const filterByValue = (event) => {
+    const type = event.target.value;
+    if(type == "remote"){
+        const remoteJobs = appliedJobs.filter(job => job.jobtype[0] == type);
+        setJobWithType(remoteJobs);
+    }
+    else if(type == "onsite"){
+        const onsiteJobs = appliedJobs.filter(job => job.jobtype[0] == type);
+        setJobWithType(onsiteJobs);
+    }
+  }
   return (
     <div>
       <div className="bg-violet-50 py-10">
         <h2 className="text-3xl text-center font-bold">Applied Jobs</h2>
       </div>
+      <div>
+      <select className="select w-28" onChange={filterByValue}>
+        <option disabled selected>Filter by</option>
+        <option value="remote">Remote</option>
+        <option value="onsite">Onsite</option>
+      </select>
+      </div>
       <div className="container mx-auto px-5 grid grid-cols-1 my-14 gap-10 flex-col items-center justify-center">
-        {appliedJobs.map((job) => (
+        {jobWithType.map((job) => (
           <RenderJob key={job.id} job={job}></RenderJob>
         ))}
       </div>
