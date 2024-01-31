7201d3ba967b0582536bcff9256ae7a25a3efeb0
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-01 00:44:50 +0600

    Statistics changed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
+const data01 = [
+  { name: "A-1", mark: 59 },
+    { name: "A-2", mark: 60 },
+    { name: "A-3", mark: 60 },
+    { name: "A-4", mark: 60 },
+    { name: "A-5", mark: 60 },
+    { name: "A-6", mark: 60 },
+    { name: "A-7", mark: 60 },
+    { name: "A-8", mark: 60 },
+];
+
+
 
 const Statistics = () => {
   const data = [
@@ -25,28 +31,19 @@ const Statistics = () => {
         <h2 className="text-3xl text-center font-bold">My Assignment marks</h2>
       </div>
       <div className="container mx-auto flex h-96 items-center justify-center">
-        <AreaChart
-          width={380}
-          height={300}
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area
-            type="monotone"
+      <PieChart width={400} height={400}>
+          <Pie
             dataKey="mark"
-            stroke="#8884d8"
+            isAnimationActive={false}
+            data={data01}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
             fill="#8884d8"
+            label
           />
-        </AreaChart>
+          <Tooltip />
+        </PieChart>
       </div>
     </div>
   );
